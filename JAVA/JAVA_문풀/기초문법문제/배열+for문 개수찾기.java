1e836f09773325f273c3d0be2527cc6cb4c65fde
YangDaEun <das03041@gmail.com>
2022-04-04 17:56:30 +0900

    fix : 파일 확장자 변경

--- JAVA/JAVA_문풀/기초문법문제/배열+for문 개수찾기.java
+++ JAVA/JAVA_문풀/기초문법문제/배열+for문 개수찾기.java
@@ -1,6 +1,6 @@
 1. 1부터 10까지 짝수, 홀수 판단.
 
-```java
+
   	for(int i=0; i<=10; i++) {
 			if(i%2 == 0) {
 				System.out.println("짝수는 : " + i);
@@ -13,9 +13,9 @@
 
 결과 : 4
 
-```
+
 2. 1부터 100까지 짝수 개수 구하기.
-```java
+
 	int count = 0;
 	int count2 = 0;
 	for(int i=0; i<=10; i++) {
@@ -30,10 +30,10 @@
 		System.out.println("홀수는 : " + count2);
 		
 
-```
+
 3. 배열을 선언하고 100이 넘는 개수가 몇개인지 찾으시오.
 
-```java
+
     public static void main(String[] args) {
 		   int count = 0;
 		   int array[] = {101, 110, 120, 130, 88, 30};
@@ -46,9 +46,8 @@
 		   System.out.println(count);
 	}
 
-```
 4. 1부터 10까지 더한값 구하기 
-```java
+
 		int sum = 0;
 		for(int i=0; i<=10; i++) {
 			sum += i;
