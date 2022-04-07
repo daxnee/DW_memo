eecf15431636cc0979cfa0daef92d6d613be4d2e
YangDaEun <das03041@gmail.com>
2022-04-07 20:35:00 +0900

    feat : 카페적립 코드 완성

--- JAVA/JAVA_문풀/생성자/0406_포인트/Cafe.java
+++ JAVA/JAVA_문풀/생성자/0406_포인트/Cafe.java
@@ -1,65 +1,63 @@
-// public class Cafe {
-// 	int count;
-// 	int totalCup;
-	
-// 	public int orderCoffee() { // 주문
-// 		System.out.println(count + "잔 주문");
-// 		return count;
-// 	}
-	
-// 	public void savePoint(int count) { // 적립
-		
-// 		System.out.println(totalCup + "잔 적립");
-// 		this.totalCup += count;
-// 	}
-	
-// 	public static void main(String[] args) {
-// 		// 커피를 마시다
-// 		// 포인트를 적립하다
-// 		Cafe c = new Cafe();
-		
-// 		c.orderCoffee(5);
-// 		c.savePoint();
-// }
-// }
-
+package study;
 
 public class Cafe {
 	int count;
 	int totalCup;
 	String rank;
 
-		public int getCount() {
-			return count;
-		}
+	
+	public int getCount() {
+		return count;
+	}
 
-
-		public void setCount(int count) {
-			System.out.println(count+"잔 주문"); // 주문 
-			this.count += count;
-			
-		}
-
-		public int getTotalCup() {
-			return totalCup;
-		}
-
-
-		public void setTotalCup(int totalCup) { // 적립
-			this.totalCup += count;
-			this.totalCup += totalCup;
-			System.out.println(totalCup +"잔 적립"); 
-		}
-
-
-		public static void main(String[] args) {
-			// 커피를 마시다
-			// 포인트를 적립하다
-			Cafe c = new Cafe();
-			c.setCount(5);
-			
-			System.out.println("적립 : " + c.getTotalCup());
-			
+	public void setCount(int count) {
+		
+		this.count = count;
+		totalCup += count;
+		System.out.println(count+"잔 주문"); // 주문 
 		
 	}
+
+	public int getTotalCup(int totalCup) {
+		return totalCup;
 	}
+
+
+	public void setTotalCup() { // 적립
+		this.totalCup = totalCup;
+		System.out.println(totalCup +"잔 적립"); 
+	}
+	
+	public void CheckRank() {
+		if(totalCup >0 && totalCup <= 10) {
+			System.out.println("브론즈 등급입니다.");
+		}
+		
+		if(totalCup > 10 && totalCup <= 20) {
+			System.out.println("실버 등급입니다.");
+		}
+		
+		if(totalCup > 20 ) {
+			System.out.println("골드 등급입니다.");
+		}
+	}
+
+
+	public static void main(String[] args) {
+		// 커피를 마시다
+		// 포인트를 적립하다
+		Cafe c = new Cafe();
+		
+		c.setCount(5);
+		c.getCount();
+		c.setTotalCup();
+		c.setCount(5);
+		c.getCount();
+		c.setTotalCup();
+		c.CheckRank();
+		c.setCount(10);
+		c.setTotalCup();
+		c.CheckRank();
+	
+} 
+}
